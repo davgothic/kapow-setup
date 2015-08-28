6ca8fa7814883d04f8bee2071326c9369b072bb5
Dave Green <hello@davetgreen.me>
2015-08-28 16:16:38 +0100

    Finished bash script

---
+++ kapow.sh
@@ -0,0 +1,58 @@
+# Kapow! Team...ASSEMBLE!!!
+# -----------------------------------------------------------------------------
+
+# GitHub Kapow URL prefix
+prefix="https://github.com/mkdo/kapow-";
+suffix="/archive/master.zip";
+
+# Array of Kapow! component names
+declare -a arr=("skeleton" "sass" "grunt" "theme")
+
+# Fetch and extract the archives
+for i in "${arr[@]}"
+do
+  file="$i.zip"
+
+  curl -Lo $file $prefix$i$suffix
+
+  unzip $file
+done
+
+# Move Skeleton
+skeldir="kapow-skeleton-master";
+if [ -d $skeldir ]
+  then
+  cp -a $skeldir/* kapow/
+fi
+
+# Move Sass
+sassdir="kapow-sass-master";
+if [ -d $sassdir ]
+  then
+  cp -a $sassdir/* kapow/assets/sass
+fi
+
+# Move Grunt
+gruntdir="kapow-grunt-master";
+if [ -d $gruntdir ]
+  then
+  cp -a $gruntdir/* kapow/
+fi
+
+# Move Theme
+themedir="kapow-theme-master";
+if [ -d $themedir ]
+  then
+  mkdir kapow/htdocs/wp-content/themes/my-project
+  cp -a $themedir/* kapow/htdocs/wp-content/themes/my-project
+fi
+
+# Remove the archives
+rm *.zip
+rm -r kapow-*
+
+# Install deps
+
+cd kapow
+
+npm install && bower install
