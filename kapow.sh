dbd9aea9191631be8dce8f74264cb4d9c7bcd9c6
Dave Green <hello@davetgreen.me>
2015-10-19 13:51:32 +0100

    Added string replacements and other tweaks

--- kapow.sh
+++ kapow.sh
@@ -1,5 +1,12 @@
-# Kapow! Team...ASSEMBLE!!!
+# Kapow! Setup Script - Requires Bash shell on OSX
 # -----------------------------------------------------------------------------
+
+# Input variables
+slug=$1
+nicename=$2
+dbname=$3
+authorname=$4
+authoremail=$5
 
 # GitHub Kapow URL prefix
 prefix="https://github.com/mkdo/kapow-";
@@ -10,46 +17,91 @@ declare -a arr=("skeleton" "sass" "grunt" "theme")
 
 # Fetch and extract the archives
 for i in "${arr[@]}"
-do
-  file="$i.zip"
+	do
+		file="$i.zip"
 
-  curl -Lo $file $prefix$i$suffix
+		curl -Lo $file $prefix$i$suffix
 
-  unzip $file
+		unzip $file
 done
 
 # Move Skeleton
-skeldir="kapow-skeleton-master";
+skeldir="kapow-skeleton-master/kapow-skeleton";
 if [ -d $skeldir ]
-  then
-  cp -a $skeldir/* /
+	then
+	cp -r $skeldir/* .
+	cp -r $skeldir/.gitignore .
 fi
 
 # Move Sass
-sassdir="kapow-sass-master";
+sassdir="kapow-sass-master/kapow-sass";
 if [ -d $sassdir ]
-  then
-  cp -a $sassdir/* assets/sass
+	then
+	cp -a $sassdir/* assets/sass
 fi
 
 # Move Grunt
-gruntdir="kapow-grunt-master";
+gruntdir="kapow-grunt-master/kapow-grunt";
 if [ -d $gruntdir ]
-  then
-  cp -a $gruntdir/* /
+	then
+	cp -a $gruntdir/* .
 fi
 
 # Move Theme
-themedir="kapow-theme-master";
+themedir="kapow-theme-master/kapow-theme";
 if [ -d $themedir ]
-  then
-  mkdir build/wp-content/themes/my-project
-  cp -a $themedir/* build/wp-content/themes/my-project
+	then
+
+	if [ ! $slug ]
+	then
+	slug="my-project"
+	fi
+
+	mkdir build/wp-content/themes/$slug
+	chmod 755 build/wp-content/themes/$slug
+	cp -a $themedir/* build/wp-content/themes/$slug
 fi
 
 # Remove the archives
 rm *.zip
 rm -r kapow-*
+
+# String replacements using input variables
+for file in `find .  -type f ! -name 'kapow.sh' ! -name '.DS_Store' ! -name '*.png' ! -name '*.mo'`; do
+	if [[ -f $file ]] && [[ -w $file ]]; then
+
+		# Slug
+		if [[ $slug ]]
+		then
+		sed -i "" "s/my-project/$slug/g" "$file"
+		fi
+
+		# Nice Name
+		if [[ $nicename ]]
+		then
+		sed -i "" "s/My Project/$nicename/g" "$file"
+		fi
+
+		# DB Name
+		if [[ $dbname ]]
+		then
+		sed -i "" "s/my_project/$dbname/g" "$file"
+		fi
+
+		# Author Name
+		if [[ $authorname ]]
+		then
+		sed -i "" "s/Author Name/$authorname/g" "$file"
+		fi
+
+		# Author Email
+		if [[ $authoremail ]]
+		then
+		sed -i "" "s/hello@my-project.com/$authoremail/g" "$file"
+		fi
+
+	fi
+done
 
 # Install All The Things(tm)
 npm install && bower install && composer install
